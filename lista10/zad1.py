9235686ed4d89d02df30aba250e850450f64f0ad
InevitableShot <InevitableShot321@gmail.com>
2021-01-21 08:49:03 +0100

    zadania

--- lista10/zad1.py
+++ lista10/zad1.py
@@ -1,13 +1,14 @@
+from math import pi
+
+
 class Kolo:
     def __init__(self, promien):
         self.promien = promien
 
     def pole(self):
-        from math import pi
         return pi*self.promien**2
 
     def obwod(self):
-        from math import pi
         return 2*pi*self.promien
 
 
